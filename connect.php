c34650bc188d73db97eb8934685a0c15767cc9fe
waghorn <waghorn.chris@hotmail.co.uk>
2015-12-05 00:44:53 +0000

    Connect to database

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('127.0.0.1', 'root', 'code4good');
+$conn = mysqli_connect('127.0.0.1', 'root', 'code4good', 'Route21');
 
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
