e6332b089370184b9e67c4993bff06741b313377
georgiai <georgiaisaac@hotmail.co.uk>
2015-12-05 05:47:23 +0000

    Update opportunities.php

--- opportunities.php
+++ opportunities.php
@@ -9,6 +9,104 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
+        <style>
+        .vcenter {
+            display: inline-block;
+            vertical-align: middle;
+            float: none;
+        }
+        
+        body {
+            padding: 50px;
+        }
+        
+        #login-dp {
+            min-width: 250px;
+            padding: 14px 14px 0;
+            overflow: hidden;
+            background-color: rgba(255, 255, 255, .8);
+        }
+        
+        #login-dp .help-block {
+            font-size: 12px
+        }
+        
+        #login-dp .bottom {
+            background-color: rgba(255, 255, 255, .8);
+            border-top: 1px solid #ddd;
+            clear: both;
+            padding: 14px;
+        }
+        
+        #login-dp .social-buttons {
+            margin: 12px 0
+        }
+        
+        #login-dp .social-buttons a {
+            width: 49%;
+        }
+        
+        #login-dp .form-group {
+            margin-bottom: 10px;
+        }
+        
+        .btn-fb {
+            color: #fff;
+            background-color: #3b5998;
+        }
+        
+        .btn-fb:hover {
+            color: #fff;
+            background-color: #496ebc
+        }
+        
+        .btn-tw {
+            color: #fff;
+            background-color: #55acee;
+        }
+        
+        .btn-tw:hover {
+            color: #fff;
+            background-color: #59b5fa;
+        }
+        
+        @media(max-width:768px) {
+            #login-dp {
+                background-color: inherit;
+                color: #fff;
+            }
+            #login-dp .bottom {
+                background-color: inherit;
+                border-top: 0 none;
+            }
+        }
+        
+        #search {
+            width: 300px;
+            height: 50px;
+            margin: auto;
+        }
+        
+        .navbar-brand-centered {
+            position: absolute;
+            left: 50%;
+            display: block;
+            text-align: center;
+        }
+        
+        #searchBtn {
+            position: relative;
+            left: 310px;
+            top: -55px;
+        }
+        
+        .table-hover tbody tr:hover td,
+        .table-hover tbody tr:hover th {
+            background-color: #eeeeea;
+        }
+
+    </style>
+
         <title>Opportunities - Route21</title>
 
         <!-- Bootstrap Core CSS -->
@@ -29,17 +127,69 @@
 
     <body>
 
-       <!-- Navigation -->
-        <nav class="navbar navbar-inverse" role="navigation">
-            <div class="container">
+       
+	 <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-inverse" role="navigation">
+        <div class="container-fluid">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/homepage.html">Homepage</a></li>
                     <li><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/search.php">Search for Jobs</a></li>
                 </ul>
-                <!-- /.navbar-collapse -->
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <p class="navbar-text">Already have an account?</p>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                        <ul id="login-dp" class="dropdown-menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div style="text-align:center;">Login via</div>
+                                        <div class="social-buttons">
+                                            <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                                            <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                                        </div>
+                                        <div style="text-align:center;">or</div>
+                                        <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                                <div class="help-block text-right"><a href="">Forget your password?</a></div>
+
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <div style="text-align:center;margin-left:auto;margin-right:auto;">
+                                                        <input type="checkbox"> keep me logged-in</div>
+
+                                                </label>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="bottom text-center">
+                                        New here ? <a href="#"><b>Join Us</b></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
-            <!-- /.container -->
-        </nav>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
 
         <!-- Page Content -->
         <div class="container">
