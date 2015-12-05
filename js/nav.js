13e25dd11b2be0a14bee7deadf915fc69951d51d
SirAmikVase <jimbobarinio@gmail.com>
2015-12-05 10:07:12 +0000

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,14 @@
+//Login script for the nav bar drop down menu.
+
 $(function() {
     $('#login-nav').submit(function(event) {
+        //Assign values of password and email fields to a temporary variable.
         var formData = {
             'email': $('#exampleInputEmail2').val(),
             'password': $('#exampleInputPassword2').val()
         };
-
+        
+        //Send the values of the password and email fields to the login control script.
         $.ajax({
             type: 'POST',
             url: 'php/login.php',
@@ -22,11 +26,11 @@ $(function() {
                 }
             }
         })
-
         event.preventDefault();
     });
+      //End
     
-    
+    //If the logout button is pressed then the logout function is called.
     $('#logout').click(function() {
         $.ajax({
             type: 'POST',
