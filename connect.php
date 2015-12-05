0d2d75fe17c725a0e3aef01c0f014a688ec98fd8
waghorn <waghorn.chris@hotmail.co.uk>
2015-12-05 00:42:18 +0000

    Connection test

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect('127.0.0.1', 'root', 'code4good');
 
 if (!$conn) {
-	echo die("Connection failed: ".mysqli_connect_error());
+	die("Connection failed: ".mysqli_connect_error());
 }
 echo "Connected";
 ?>
