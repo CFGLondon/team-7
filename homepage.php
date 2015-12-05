e6ecbd5faae37d5e2a5d2b5e1698ce8b6ea33f42
Jamie932 <Jamie932@gmail.com>
2015-12-05 06:56:00 +0000

    Added navbar to independent file

--- homepage.php
+++ homepage.php
@@ -2,119 +2,6 @@
 <html lang="en">
 
 <head>
-    <style>
-        .vcenter {
-            display: inline-block;
-            vertical-align: middle;
-            float: none;
-        }
-        
-        
-        table.dataTable tr.odd td.sorting_1 {
-             background-color: #dcbe23 !important;
-        }
-        
-        body {
-            padding: 50px;
-        }
-        
-        #login-dp {
-            min-width: 250px;
-            padding: 14px 14px 0;
-            overflow: hidden;
-            background-color: rgba(255, 255, 255, .8);
-        }
-        
-        #login-dp .help-block {
-            font-size: 12px
-        }
-        
-        #login-dp .bottom {
-            background-color: rgba(255, 255, 255, .8);
-            border-top: 1px solid #ddd;
-            clear: both;
-            padding: 14px;
-        }
-        
-        #login-dp .social-buttons {
-            margin: 12px 0
-        }
-        
-        #login-dp .social-buttons a {
-            width: 49%;
-        }
-        
-        #login-dp .form-group {
-            margin-bottom: 10px;
-        }
-        
-        .btn-fb {
-            color: #fff;
-            background-color: #3b5998;
-        }
-        
-        .btn-fb:hover {
-            color: #fff;
-            background-color: #496ebc
-        }
-        
-        .btn-tw {
-            color: #fff;
-            background-color: #55acee;
-        }
-        
-        .btn-tw:hover {
-            color: #fff;
-            background-color: #59b5fa;
-        }
-        
-        @media(max-width:768px) {
-            #login-dp {
-                background-color: inherit;
-                color: #fff;
-            }
-            #login-dp .bottom {
-                background-color: inherit;
-                border-top: 0 none;
-            }
-        }
-        
-        #search {
-            width: 300px;
-            height: 50px;
-            margin: auto;
-        }
-        
-        .navbar-brand-centered {
-            position: absolute;
-            left: 50%;
-            display: block;
-            text-align: center;
-        }
-        
-        #searchBtn {
-            position: relative;
-            left: 310px;
-            top: -55px;
-        }
-        
-        .table-hover tbody tr:hover td,
-        .table-hover tbody tr:hover th {
-            background-color: #eeeeea;
-        }
-        
-        .navbar-right {
-            margin-right: 0px;
-            padding-top: 10px;
-            color: #fff;
-        }
-        
-        .navbar-right a {
-            color: #fff !important;
-            font-weight: bold;
-        }
-
-    </style>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -135,7 +22,8 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/business-frontpage.css" rel="stylesheet">
+    <link href="css/business-frontpage.css" rel="stylesheet">    
+    <link href="css/navbar.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -180,96 +68,8 @@
 </head>
 
 <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-inverse" role="navigation">
-        <div class="container-fluid">
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/homepage.php">Homepage</a></li>
-                    <li><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/search.php">Search for Jobs</a></li>
-                </ul>
-
-            <?php
-                session_start();
-                include 'connect.php';
-
-                if (!isset($_SESSION['loggedinUser'])) {
-            ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <p class="navbar-text">Already have an account?</p>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-                        <ul id="login-dp" class="dropdown-menu">
-                            <li>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div style="text-align:center;">Login via</div>
-                                        <div class="social-buttons">
-                                            <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                                            <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                                        </div>
-                                        <div style="text-align:center;">or</div>
-                                        <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                            <div class="form-group">
-                                                <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                                <div class="help-block text-right"><a href="">Forget your password?</a></div>
-
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <div style="text-align:center;margin-left:auto;margin-right:auto;">
-                                                        <input type="checkbox"> keep me logged-in</div>
-
-                                                </label>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <div class="bottom text-center">
-                                        New here ? <a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/register.php"><b>Join Us</b></a>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                
-              <?php
-                    } else {
-                ?>
-                
-               <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <p>Welcome back, 
-                        
-                        <?php     
-                            $query = "SELECT * FROM user WHERE user_id = " . $_SESSION['loggedinUser'];
-                            $userrow = mysqli_query($conn, $query); //Execute query.
-
-                            while ($row = $userrow->fetch_assoc()) {
-                                echo "<a href='profile.php?id=" . $row['user_id'] . "'>" . $row['first_name'] . "</a>!";
-                            }
-                        ?></p>
-                    </li>
-                </ul>
-                
-                <?php } ?>  
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+    <?php include 'navbar.php'; ?>
+ 
     <header class="business-header">
         <div class="container">
             <div class="row">
--- navbar.php
+++ navbar.php
@@ -1,31 +1,88 @@
-<nav class="navbar navbar-inverse" role="navigation">
-            <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-				<img src = "Assets/R21 Logo Light.png" alt = "Route 21" height = "50px" width = "150px">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Search</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Services</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
+   <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-inverse" role="navigation">
+        <div class="container-fluid">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/homepage.php">Homepage</a></li>
+                    <li><a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/search.php">Search for Jobs</a></li>
+                </ul>
+
+            <?php
+                session_start();
+                include 'connect.php';
+
+                if (!isset($_SESSION['loggedinUser'])) {
+            ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <p class="navbar-text">Already have an account?</p>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                        <ul id="login-dp" class="dropdown-menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div style="text-align:center;">Login via</div>
+                                        <div class="social-buttons">
+                                            <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                                            <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                                        </div>
+                                        <div style="text-align:center;">or</div>
+                                        <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                                <div class="help-block text-right"><a href="">Forget your password?</a></div>
+
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <div style="text-align:center;margin-left:auto;margin-right:auto;">
+                                                        <input type="checkbox"> keep me logged-in</div>
+
+                                                </label>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="bottom text-center">
+                                        New here ? <a href="http://ec2-54-78-185-241.eu-west-1.compute.amazonaws.com/team7/register.php"><b>Join Us</b></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                
+              <?php
+                    } else {
+                ?>
+                
+               <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <p>
+                        <?php     
+                            $query = "SELECT * FROM user WHERE user_id = " . $_SESSION['loggedinUser'];
+                            $userrow = mysqli_query($conn, $query); //Execute query.
+
+                            while ($row = $userrow->fetch_assoc()) {
+                                echo "<a href='profile.php?id=" . $row['user_id'] . "'>" . $row['first_name'] . "</a>";
+                            }
+                        ?></p>
+                    </li>
+                </ul>
+                
+                <?php } ?>  
             </div>
-            <!-- /.container -->
-        </nav>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
