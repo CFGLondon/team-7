58578d1f64224d8e7335f4e1d7eef41fb5bee4dc
Jamie932 <Jamie932@gmail.com>
2015-12-05 08:40:38 +0000

    Removed Script tags

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,28 @@
-    <script>
-        $(function() {
-            $('#login-nav').submit(function(event) {
-                var formData = {
-                    'email': $('#exampleInputEmail2').val(),
-                    'password': $('#exampleInputPassword2').val()
-                };
+$(function() {
+    $('#login-nav').submit(function(event) {
+        var formData = {
+            'email': $('#exampleInputEmail2').val(),
+            'password': $('#exampleInputPassword2').val()
+        };
 
-                $.ajax({
-                    type: 'POST',
-                    url: 'php/login.php',
-                    data: formData,
-                    dataType: 'json',
-                    encode: true,
-                    error: function(request, status, error) {
-                        console.log(request.responseText);
-                    },
-                    success: function(data) {
-                        if (data.error) {
-                            console.log(data.error);
-                        } else {
-                            window.location.replace("homepage.php");
-                        }
-                    }
-                })
+        $.ajax({
+            type: 'POST',
+            url: 'php/login.php',
+            data: formData,
+            dataType: 'json',
+            encode: true,
+            error: function(request, status, error) {
+                console.log(request.responseText);
+            },
+            success: function(data) {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    window.location.replace("homepage.php");
+                }
+            }
+        })
 
-                event.preventDefault();
-            });
-        });
-
-    </script>
+        event.preventDefault();
+    });
+});
