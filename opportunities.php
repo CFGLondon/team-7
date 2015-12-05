9befe8e6717cc356c18973b045b02f8d34595c07
Marc Hunt <BluntToothbrush@gmail.com>
2015-12-05 04:16:58 +0000

    Opportunities Page

---
+++ opportunities.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Opportunities - Route21</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/business-frontpage.css" rel="stylesheet">
+        <link href="css/opport.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse" role="navigation">
+            <div class="container">
+                
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+            
+            <div id="leftBar">
+                <div class="companyImage"><img src="https://pbs.twimg.com/profile_images/461908736106168320/S9sGEWmY.png" class="companyLogo"></div>
+                <div class="companyDetails">
+                    <p class="companyDeets"><b>Company Name: </b>JP Morgan</p>
+                    <p class="companyDeets"><b>Opportunity Title: </b>Senior Programmer</p> 
+                    <p class="companyDeets"><b>Opportunity Type: </b>10+ Week Placement</p>
+        <p class="companyDeets"><b>Starting Date: </b>June 2016</p>
+        <p class="companyDeets"><b>Salary: </b>£19000</p>
+<p class="companyDeets"><b>Skills required: </b>PHP, Unix, MySQL, HTML, CSS, Python</p>
+<p class="companyDeets"><b>Sector: </b>Technology</p>
+                </div>    
+                <div id="companyShortDescri"><h4>Opportunity Description</h4><p>This job is well cool your friends will be jelly if you get it omg good luck :)</p></div>
+            </div>
+            
+            <hr>
+
+            <div id="mainContent">
+                <div class="jobMainDescription"><h3>Job Description</h3><p>This will be a varied role working within the competence & Learning department of JP Morgan and will include a variety of tasks including:
+
+    Daily maintenance of training tools and processes
+    Liaising with training providers and academies Dealing with queries from internal customers
+    Completing learning and competence analysis
+    Course design and development
+    Training provider selection process
+    Involvement in Government funding project
+    Data collection - Compilation of, and coordinating the data collection process. Producing weekly data analysis reports
+    Liaising with external accountant
+</p></div>
+            </div>
+
+
+            <hr>
+
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Your Website 2014</p>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </footer>
+
+        </div>
+        <!-- /.container -->
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+
+    </html>
