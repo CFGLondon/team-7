fbf9253e0273a282534bcf995343a350d72cda48
waghorn <waghorn.chris@hotmail.co.uk>
2015-12-05 00:52:27 +0000

    Connect to database Test 2

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect('127.0.0.1', 'root', 'code4good', 'Route21');
+$conn = mysqli_connect('127.0.0.1', 'root', 'code4good');
+mysqli_select_db($conn, 'Route21');
 
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
