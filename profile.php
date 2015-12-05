4ece355cff1a4d2122bfdbc30a24626d230f89c9
Marc Hunt <BluntToothbrush@gmail.com>
2015-12-05 07:24:52 +0000

    Replace rightwork

--- profile.php
+++ profile.php
@@ -120,7 +120,7 @@
                                 while ($row = $eventRow->fetch_assoc()) {
                                 for(int i= 1; i >2; i++) { 
                                     echo '<div class="leftWork"><div class="jobTitle">'.$row['name'].'</div><div class="jobLength">'.$row['type'].' - '.$row['location'] .'</div><div class="jobDesc"><b>Job Description -</b> '.$row['about'].' </div></div>';
-                                    echo '<div class="rightWork"><div class="jobTitle">Housekeeper</div><div class="jobLength">June 1997 - September 2012</div><div class="jobDesc"><b>Job Description -</b></div></div>';
+                    echo '<div class="rightWork"><div class="jobTitle">'.$row['name'].'</div><div class="jobLength">'.$row['type'].' - '.$row['location'] .'</div><div class="jobDesc"><b>Job Description -</b> '.$row['about'].' </div></div>';
                                 }
                                 }
                             }
